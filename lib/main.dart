8d287079f0777a950aa97f36fd6a28d523cce44d
Jerry Hou <jerry.hou0223@gmail.com>
2021-03-09 01:30:47 -0500

    basic questions and created question custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,53 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
+    print("Answer Chosen");
+    print(questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    //build is always run and updates
+    var questions = [
+      "what is your favorte color?",
+      "what is your least favorite color?",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MyFirstApp'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print("Answer2 chosen"),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print('answer 3 chosen');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
